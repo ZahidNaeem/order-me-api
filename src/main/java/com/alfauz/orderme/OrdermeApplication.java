95d32c2df32618165b7739877aface14c8078c73
Zahid Naeem <hzahidnaeem@gmail.com>
2020-04-15 04:23:56 +0500

    logs level set to debug

--- src/main/java/com/alfauz/orderme/OrdermeApplication.java
+++ src/main/java/com/alfauz/orderme/OrdermeApplication.java
@@ -1,13 +1,18 @@
 package com.alfauz.orderme;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrdermeApplication {
 
+    private static final Logger LOG = LogManager.getLogger(OrdermeApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(OrdermeApplication.class, args);
+        LOG.debug("==============================================Debug===========================================");
+        LOG.info("==============================================info============================================");
     }
-
 }
