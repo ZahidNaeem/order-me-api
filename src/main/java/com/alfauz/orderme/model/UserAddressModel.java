79e4bfa7b822679b1b7174cbf259be8610d10ecf
Zahid Naeem <hzahidnaeem@gmail.com>
2020-04-17 14:11:47 +0500

    some constraints added on UserAddressModel

--- src/main/java/com/alfauz/orderme/model/UserAddressModel.java
+++ src/main/java/com/alfauz/orderme/model/UserAddressModel.java
@@ -5,13 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserAddressModel {
     private Long id;
+
+    @NotBlank(message = "Address must not be blank")
+    @Size(min = 1, max = 200, message = "Address should has min. 1 and max. 200 characters")
     private String address;
+
+    @NotBlank(message = "Address Type must not be blank")
+    @Size(min = 1, max = 20, message = "Address should has min. 1 and max. 20 characters")
     private String addressType;
+
     private Long user;
 }
